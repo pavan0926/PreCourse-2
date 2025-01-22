bac7616497b73b7d4633931206bee0ce63f0add8
pavan0926 <bandarupavan99@gmail.com>
2025-01-21 18:06:11 -0800

    Done precourse2

--- Exercise_3.java
+++ Exercise_3.java
@@ -18,8 +18,17 @@ class LinkedList
    //Complete this function
     void printMiddle() 
     { 
-        //Write your code here
-	//Implement using Fast and slow pointers
+         //Write your code here
+         Node slow_ptr = head;
+        Node fast_ptr = head;
+        
+        if (head != null) {
+            while (fast_ptr != null && fast_ptr.next != null) { //Implement using Fast and slow pointers
+                fast_ptr = fast_ptr.next.next;
+                slow_ptr = slow_ptr.next;
+            }
+            System.out.println("The middle element is [" + slow_ptr.data + "]");
+        }
     } 
   
     public void push(int new_data) 
--- Exercise_5.java
+++ Exercise_5.java
@@ -1,22 +1,57 @@
-class IterativeQuickSort { 
-    void swap(int arr[], int i, int j) 
+  class IterativeQuickSort 
     { 
-	//Try swapping without extra variable 
+	void swap(int arr[], int i, int j) {
+        if (i != j) {  //Try swapping without extra variable 
+            arr[i] = arr[i] + arr[j];
+            arr[j] = arr[i] - arr[j];
+            arr[i] = arr[i] - arr[j];
     } 
+    }
   
     /* This function is same in both iterative and 
        recursive*/
     int partition(int arr[], int l, int h) 
     { 
-        //Compare elements and swap.
-    } 
+       int pivot = arr[h];
+        int i = (l - 1); 
+        for (int j = l; j < h; j++) {
+            
+            if (arr[j] <= pivot) {
+                i++;
+
+                // swap arr[i] and arr[j]
+                swap(arr, i, j);
+            }
+        }
+        // Sorts arr[l..h] using iterative QuickSort 
+         swap(arr, i + 1, h);
+        return i + 1;
+    }
+    
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
-        //Try using Stack Data Structure to remove recursion.
-    } 
-  
+        int[] stack = new int[h - l + 1];
+          int top = -1; //Try using Stack Data Structure to remove recursion.
+       stack[++top] = l;
+        stack[++top] = h;
+        while (top >= 0) 
+        {
+            h = stack[top--];
+            l = stack[top--];
+            int p = partition(arr, l, h);
+            if (p - 1 > l) {
+                stack[++top] = l;
+                stack[++top] = p - 1;
+            }
+            if (p + 1 < h) 
+            {
+                stack[++top] = p + 1;
+                stack[++top] = h;
+            }
+        }
+    }
     // A utility function to print contents of arr 
     void printArr(int arr[], int n) 
     { 
